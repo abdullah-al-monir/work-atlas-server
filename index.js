d1b4304c80f0b70fdc64b89fcf41f553cf169216
MrColonThree <abdullahalmonir364@gmail.com>
2023-11-06 02:18:31 +0600

    Used cookie parser as middleware and create a middleware named verifyToken ,created jwt  and logout api to ensure that if there is no user then the page will be secured or unavailabe.Get the filtered data from appliedJobs api by email and category

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -7,7 +9,27 @@ const port = process.env.PORT || 7000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a4caxts.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +47,7 @@ async function run() {
 
     const categoryCollection = client.db("Categorires").collection("Category");
     const jobCollection = client.db("JobsDB").collection("Jobs");
+    const appliedJobCollection = client.db("JobsDB").collection("appliedJobs");
 
     app.get("/categories", async (req, res) => {
       const cursor = categoryCollection.find();
@@ -49,7 +72,47 @@ async function run() {
       const result = await jobCollection.findOne(query);
       res.send(result);
     });
+    app.get("/appliedJobs", verifyToken, async (req, res) => {
+      const userEmail = req.query?.email;
+      const query = { email: userEmail };
+      const category = req.query.category;
+      if (category && category !== "All Jobs") {
+        query.category = category;
+      }
+      const result = await appliedJobCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/appliedJobs", async (req, res) => {
+      const appliedJob = req.body;
+      const result = await appliedJobCollection.insertOne(appliedJob);
+      res.send(result);
+    });
 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("Logged out", user);
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ status: true });
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
